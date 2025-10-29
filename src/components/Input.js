b4258ca937e3045587dc860e0015c4796676fc3d
Rahulllkr <rahulkumar8604745@gmail.com>
2025-10-29 14:36:57 +0530

    check

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,15 @@
 
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
 const Input = ({text,setText}) => {
   return (
     <div className='textarea' style={{display:"flex",alignItems:"center"}}>
         <h1>#</h1>
+        <ReactMarkdown>
         <textarea onChange={(e) => setText(e.target.value)}>
         </textarea>
+        </ReactMarkdown>
     </div>
   )
 }
